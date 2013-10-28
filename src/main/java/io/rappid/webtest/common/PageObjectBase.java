f3efc95ca2776d91e667048a9e23c0d7d230fb65
Tony Findeisen <github@it-ony.de>
2013-10-28 21:54:00 +0100

    implemented WebElement methods

--- src/main/java/io/rappid/webtest/common/PageObjectBase.java
+++ src/main/java/io/rappid/webtest/common/PageObjectBase.java
@@ -1,10 +1,10 @@
 package io.rappid.webtest.common;
 
 import com.google.common.base.Function;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.interactions.internal.Coordinates;
+import org.openqa.selenium.internal.Locatable;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,7 +16,7 @@ import java.util.List;
  * Date: 14.10.13
  * Time: 00:03
  */
-public abstract class PageObjectBase extends WebTestBase {
+public abstract class PageObjectBase extends WebTestBase implements WebElement, Locatable {
 
     protected final Logger log = LoggerFactory.getLogger(this.getClass());
 
@@ -124,4 +124,90 @@ public abstract class PageObjectBase extends WebTestBase {
         }
     }
 
+    @Override
+    public void click() {
+        getWebElement().click();
+    }
+
+    public void clickAt(int offsetX, int offsetY) {
+        new Actions(driver())
+                .moveToElement(this, offsetX, offsetY)
+                .click()
+                .perform();
+    }
+
+    @Override
+    public void submit() {
+        getWebElement().submit();
+    }
+
+    @Override
+    public void sendKeys(CharSequence... charSequences) {
+        getWebElement().sendKeys(charSequences);
+    }
+
+    @Override
+    public void clear() {
+        getWebElement().clear();
+    }
+
+    @Override
+    public String getTagName() {
+        return getWebElement().getTagName();
+    }
+
+    @Override
+    public String getAttribute(String s) {
+        return getWebElement().getAttribute(s);
+    }
+
+    @Override
+    public boolean isSelected() {
+        return getWebElement().isSelected();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return getWebElement().isEnabled();
+    }
+
+    @Override
+    public String getText() {
+        return getWebElement().getText();
+    }
+
+    @Override
+    public List<WebElement> findElements(By by) {
+        return getWebElement().findElements(by);
+    }
+
+    @Override
+    public WebElement findElement(By by) {
+        return getWebElement().findElement(by);
+    }
+
+    @Override
+    public boolean isDisplayed() {
+        return getWebElement().isDisplayed();
+    }
+
+    @Override
+    public Point getLocation() {
+        return getWebElement().getLocation();
+    }
+
+    @Override
+    public Dimension getSize() {
+        return getWebElement().getSize();
+    }
+
+    @Override
+    public String getCssValue(String s) {
+        return getWebElement().getCssValue(s);
+    }
+
+    @Override
+    public Coordinates getCoordinates() {
+        return ((Locatable) getWebElement()).getCoordinates();
+    }
 }
